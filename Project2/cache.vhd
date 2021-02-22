7505c6a5db9c88e759fd7b4c809e0d37bd304955
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-02-22 10:15:06 -0500

    Initial cache template

--- Project2/cache.vhd
+++ Project2/cache.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity cache is
 generic(
-	ram_size : INTEGER := 32768;
+	ram_size : INTEGER := 32768
 );
 port(
 	clock : in std_logic;
@@ -31,8 +31,51 @@ architecture arch of cache is
 
 -- declare signals here
 
+type t_state is (c_begin, c_write, c_read, write_back, memory_read);
+signal state: t_state;
+
+-- processor-cache addressing:
+-- 4 words per block = 2 bits for offset
+-- cache_data_storage_bit/block_bit :
+-- = 4096/128 = 32 blocks = 5 bits for cache_block addressing
+-- TAG = 32 - (2 + 5) = 25 bits
+
+
+-- | V  |  TAG   |  D  |  DATA |
+-- 1bit + 25bit + 1bit + 128bits
+type cache_storage_def is array (0 to 31) of std_logic_vector (154 downto 0);
+signal cache_storage: cache_storage_def;
+
+shared variable is_read: boolean := false;
+
 begin
 
 -- make circuits here
+process (clock, reset, state, s_read, s_write)
 
+-- declare other internal variables
+
+begin
+
+	if (reset = '1') then
+		state <= c_begin;
+	elsif (rising_edge(clock)) then
+	case state is
+		when c_begin =>
+		
+		when c_write =>
+		
+		when c_read =>
+		
+		when write_back =>
+		
+		when memory_read =>
+		
+		when others =>
+		NULL;
+		
+	end case;
+	end if;
+
+end process;
 end arch;
